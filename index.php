fa9a83c98319b71d451bcc8d0f2e998b911bd8d3
Muhammad Sofyan <sofyan.workspace@gmail.com>
2020-09-08 18:15:04 +0700

    Membuat file index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Office</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<a href="login.php"><Button>Login</Button></a>
+</body>
+</html>
