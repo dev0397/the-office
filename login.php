a551c4f076d0ce3ec2b973d389dab7c9a739fcf2
Muhammad Sofyan <sofyan.workspace@gmail.com>
2020-09-08 18:21:28 +0700

    Membuat halaman login.php

---
+++ login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Office</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <img class="lglogin" src="img/logo.png" alt="logo">
+    <div class="lgbox">
+        <form action="">
+        Nomer Pegawai
+        <input class="it" type="text" name="">
+        Password
+        <input class="it" type="password" name="">
+        <input class="lgsbmt" type="submit" value="Login">
+        </form>
+    </div>
+</body>
+</html>
